ffb19bd7cc98b276b1b6ef206cb6eaf8d7a98f85
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-03-07 20:58:59 +0300

    Delete app_online_shop directory

--- app_online_shop/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.apps import AppConfig
-
-class AppOnlineShopConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_online_shop'
-    verbose_name = "Объявления"
